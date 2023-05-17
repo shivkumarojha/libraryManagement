5817b1d9b118a1d6f9f9009d51a2cf2524adeecc
shivkumarojha <ojhashivkumar88@gmail.com>
2023-05-17 20:32:11 +0530

    debug true added

--- libraryManagement/settings.py
+++ libraryManagement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u9f+_g7dbf2s&mmc7=6fpl7b@*7lmgab^i%_280eq%!33zy2ha'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
